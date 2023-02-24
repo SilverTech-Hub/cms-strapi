9dabb58da08b57abcb0335b7d81d619825107111
joseprest <joserest.7io@gmail.com>
2023-02-24 00:16:57 -0500

    inital commit & deploy on vercel

---
+++ config/plugins.js
@@ -0,0 +1,18 @@
+module.exports = {
+  //
+  graphql: {
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: false,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
+  "import-export-entries": {
+    enabled: true,
+  },
+};
